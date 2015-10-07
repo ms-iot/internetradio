3c4731dd2196d8fefbabf10dc4539d8b87685560
Matt McKee <mamcke@microsoft.com>
2015-10-06 17:32:20 -0700

    add folder to group webserver files port constants file

---
+++ InternetRadioDevice/InternetRadio/WebServer/NavConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace InternetRadio
+{
+    public class NavConstants
+    {
+        public static string SETTINGS_PAGE = "settings.htm";
+        public static string HOME_PAGE = "home.htm";
+    }
+}
