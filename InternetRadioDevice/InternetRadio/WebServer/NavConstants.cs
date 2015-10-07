c690e33ef3a1ea55d1197c758b5364fc41980f32
Matt McKee <mamcke@microsoft.com>
2015-10-07 16:06:33 -0700

    began porting webhelper updated webserver to use new call signatures updated navconstants to struct to be useable in background service

--- InternetRadioDevice/InternetRadio/WebServer/NavConstants.cs
+++ InternetRadioDevice/InternetRadio/WebServer/NavConstants.cs
@@ -1,8 +1,10 @@
 ﻿namespace InternetRadio
 {
-    public class NavConstants
+    struct NavConstants
     {
         public static string SETTINGS_PAGE = "settings.htm";
         public static string HOME_PAGE = "home.htm";
+        public static string DEFAULT_PAGE = "default.htm";
+        public static string ASSETSWEB = @"Assets\Web\";
     }
 }
