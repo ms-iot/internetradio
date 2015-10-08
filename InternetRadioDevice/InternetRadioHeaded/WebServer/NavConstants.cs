f3f962582f9d4c46be377ea52012ded44e430441
Matt McKee <mamcke@microsoft.com>
2015-10-08 15:10:31 -0700

    created headed app for radio

---
+++ InternetRadioDevice/InternetRadioHeaded/WebServer/NavConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace InternetRadio
+{
+    struct NavConstants
+    {
+        public static string SETTINGS_PAGE = "settings.htm";
+        public static string HOME_PAGE = "home.htm";
+        public static string DEFAULT_PAGE = "default.htm";
+        public static string ASSETSWEB = @"Assets\Web\";
+    }
+}
