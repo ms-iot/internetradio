d6dabeea88eac0d04edfcf94287a7ab2979b8c19
HerrickSpencer <IOTMe@HerrickSpencer.com>
2015-10-12 22:53:57 -0700

    Add all functionality for adding station update layout and templates update css to bootstrap

--- InternetRadioDevice/InternetRadioHeaded/WebServer/NavConstants.cs
+++ InternetRadioDevice/InternetRadioHeaded/WebServer/NavConstants.cs
@@ -2,9 +2,11 @@
 {
     struct NavConstants
     {
-        public static string SETTINGS_PAGE = "settings.htm";
-        public static string HOME_PAGE = "home.htm";
+        public static string ASSETSWEB = @"Assets\Web";
         public static string DEFAULT_PAGE = "default.htm";
-        public static string ASSETSWEB = @"Assets\Web\";
+        public static string HOME_PAGE = "home.htm";
+        public static string SETTINGS_PAGE = "settings.htm";
+        public static string ADDSTATION_PAGE = "addstation.htm";
+        public static string ADDSTATIONSET_PAGE = "addstationset.htm";
     }
 }
